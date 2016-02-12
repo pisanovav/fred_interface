c04378510f26fd4d27033edfe3d34998ac84d28b
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2016-02-12 03:03:25 +0300

    Create test.r

---
+++ test.r
@@ -0,0 +1,7 @@
+library(RUnit)
+
+source("fred_interface.r")
+
+# WARNING: may fail on data revision, may neet to use earlier dates for testing
+checkEqualsNumeric(15961.7, get_fred_zoo('GDPCA', '2014-01-01'))
+checkEqualsNumeric(237.847, get_fred_zoo('CPIAUCSL', '2015-11-01', '2015-12-01')[2])
