b6c00217829e7eee2884d6b3b1ebfbf8127ea38d
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2016-02-12 03:09:06 +0300

    Update test.r

--- test.r
+++ test.r
@@ -5,3 +5,4 @@ source("fred_interface.r")
 # WARNING: may fail on data revision, may neet to use earlier dates for testing
 checkEqualsNumeric(15961.7, get_fred_zoo('GDPCA', '2014-01-01'))
 checkEqualsNumeric(237.847, get_fred_zoo('CPIAUCSL', '2015-11-01', '2015-12-01')[2])
+checkTrue(class(fred_to_csv('GDPCA')) == "character")
